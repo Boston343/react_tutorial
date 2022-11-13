ca420fecfe36e8e857266e6875057eccd717420d
Boston343 <branden.austin1@gmail.com>
2022-11-13 11:53:35 -0500

    challenge 3: board now creates the squares using two for loops instead of them being hardcorded

--- src/index.js
+++ src/index.js
@@ -40,34 +40,22 @@ class Board extends React.Component {
 
   render() {
     // https://reactgo.com/react-for-loop/
-    // const boardSquares = [];
-    // for (let row=0; row<3; row++) {
-    //   const boardRow = [];
-    //   for (let col=0; col<3; col++) {
-    //     boardRow.push()
-    //   }
-    //   </div>
-    // }
+    const boardSquares = [];
+    for (let row = 0; row < 3; row++) {
+      const boardRow = [];
+      for (let col = 0; col < 3; col++) {
+        boardRow.push(
+          <span key={row * 3 + col}>{this.renderSquare(row * 3 + col)}</span>
+        );
+      }
+      boardSquares.push(
+        <div className="board-row" key={row}>
+          {boardRow}
+        </div>
+      );
+    }
 
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    return <div>{boardSquares}</div>;
   }
 }
 
